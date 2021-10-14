45034278b68bd7fe0e1d7ac022a293b5b9d62bb8
Al azery <81170076+AlAzery@users.noreply.github.com>
2021-10-14 11:02:46 +0700

    Add files via upload

---
+++ E=mc2.php
@@ -0,0 +1,23 @@
+<?php
+echo("==================  Rumus Albert einstein: E = mc2 ==================");
+echo("=====================================================================");
+echo("========= konversi menghitung Energi dari masa dan kecepatan ========");
+echo("============ di ketahui E = energi, m = massa, dan c = cahaya ===========");
+
+echo("masukan nilai m = massa :");
+$m = trim(fgets(STDIN));
+echo("masukan nilai c = kecepatan cahaya :");
+$c = trim(fgets(STDIN));
+
+if ($m == "" && $c == "") {
+  echo("nilai kosong");
+  return false;
+}else{
+  //c kuadrat
+  $ck = $c*$c;
+  $E = $m*$ck;
+}
+print("**********************************************************************");
+print("maka E yang di hasilkan dari $m kg, dengan waktu $c s/m adalah ");
+print($E);
+echo(" ");
